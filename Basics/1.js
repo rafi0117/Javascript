d3e841eb8bf4d8d699dd0aa17d2e8b3d41704a2a
rafi0117 <mohdshabaaz953@gmail.com>
2022-07-19 17:19:35 +0530

    Basics and Operators

---
+++ Basics/1.js
@@ -0,0 +1,19 @@
+// Single line comment
+//console.log(123)
+
+
+/*
+Multi-line comment
+console.log("Hi CFI")
+console.log("Hi CFI")
+console.log("Hi CFI")
+console.log("Hi CFI")
+console.log("Hi CFI")
+*/
+
+
+// console.log('hi')
+// console.log('hi')
+console.log('hi', 'hi','hi')
+console.log('hi, hi, hi')
+console.log('hi')
