322e33bce38dd678f7cc7d04881df739b09e4a76
rafi0117 <mohdshabaaz953@gmail.com>
2022-07-20 10:19:22 +0530

    Bitwise Operators, Coversion tostring, Unary Operators

---
+++ Operators/8.js
@@ -0,0 +1,115 @@
+//  Bitwise Operators
+// Bitwise AND - &
+// Bitwise OR - |
+// Bitwise Not - ~
+// Bitwise XOR - ^
+
+// var a=7,b=5
+// console.log(a&b)
+// //a  b  r
+//   0  0  0
+//   0  1  0
+//   1  0  0
+//   1  1  1
+ 
+// var a=33,b=27
+// console.log(a|b)
+
+// var a=53,b=26
+// console.log(a^b)
+
+// var a=37,b=2
+// console.log(a<<b)// left shift
+
+// var a=117,b=3
+// console.log(a>>b)
+
+
+// Bitwise  NOT - ~
+// ~N = -(N+1)
+// console.log(~2)
+
+// Binary - 0bxxxx - 2
+// Decimal - nnnn - 10
+// Octal - 0oxxx - 8
+// Hexa - 0xnnn - 16
+
+// tostring
+
+// var a = 0b1000
+// console.log(a.toString(10))//decimal
+// var a = 15
+// console.log(a.toString(2))//Binary
+// var a = 10
+// console.log(a.toString(8))//Octal
+// var a = 0o10
+// console.log(a.toString(16))//hexadecimal
+
+//Unary Operator
+// +,-
+// console.log(1)
+// console.log(+1)
+// console.log(-1)
+// console.log("1")
+// console.log(+"1")
+// console.log(-"t")
+// console.log("t")
+// console.log(typeof NaN)
+
+// var a = 1, b = "1" , c = true , d = "true"
+// console.log(a, typeof a)
+// console.log(b, typeof b)
+// console.log(c, typeof c)
+// console.log(d, typeof d)
+
+// console.log(+a, typeof +a)
+// console.log(+b, typeof +b)
+// console.log(+c, typeof +c)
+// console.log(+d, typeof +d)
+
+// console.log(-a, typeof -a)
+// console.log(-b, typeof -b)
+// console.log(-c, typeof -c)
+// console.log(-d, typeof -d)
+
+//NOTE------------------
+
+// console.log(1+"1")
+
+// console.log(1+1+1+1)
+
+// console.log('1'+1+1+1)
+
+// console.log(1+1+'1'+1)
+
+// console.log(1+1+1+"1")
+
+// console.log("hello"+1+1+1)
+
+// console.log(1+1+1+"hello")
+
+// console.log(false + false)
+
+// console.log(false + true)
+
+// console.log(1 + false)
+
+// console.log(1+ true)
+
+// console.log("hi code for india")
+
+// console.log("hi code for india", "foundation")
+// console.log('morning'+"hi code for india" +"foundation" +2)
+// console.log('morning'+"hi code for india" +"foundation",2)
+
+// console.log("100" + 1+ 2 + "3" +4)
+
+// console.log(100+1+2+"500"+600)// after concatination no addition  
+
+console.log('1'-2)
+console.log(1-1-'1'+1)
+console.log(10 - "hello")
+console.log('1'-'1')
+console.log('1'-2)
+console.log(100 - 1 + '2')
+console.log('100' -1 -'2')
