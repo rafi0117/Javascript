80dc8e0c48140468ff1aa05d2740d7bacb47dd2c
rafi0117 <mohdshabaaz953@gmail.com>
2022-07-28 14:29:25 +0530

    iteration

---
+++ Iteration/15.js
@@ -0,0 +1,123 @@
+//for loop
+//for (initialisation,cond,icr/decre){
+// false/
+// }
+//for (i=0;i<=5;i++);
+
+//print 1 to 5 using loop?
+// for (i=1;i<=5;i++)
+// {
+//     console.log(i)
+// }
+//iteration 1: i=1; i<=5; 1
+//iteration 2: i=2; i<=5; 2
+//iteration 3: i=3; i<=5; 3
+//iteration 4: i=4; i<=5; 4
+//iteration 5: i=5; i<=5; 5
+//iteration 6: i=6; false;stops
+
+
+
+//print 10 to 5 using for loop?-------------------
+// for (i=10;i>=5;i--)
+// {
+//     console.log(i)
+// }
+
+// write a program to print even numbers between 50-100?
+
+// for (i=50;i<=100;i++)
+// {
+// if ((i%2)==0)// for even numbers
+// {
+//     console.log(i)
+// }
+// }
+// write a program to print odd numbers between 1-100?
+
+// for (i=1;i<=100;i++)
+// if ((i%2)!==0)
+// {
+//     console.log(i)
+// }
+
+// wap to print 10 tables?
+
+// for (i=1;i<=10;i++)
+// {
+//     console.log(`10 x ${i} = $(10 * i)`)
+// }
+
+
+// for (i=0;i<=10;i++)
+// {
+//     for (j=0;j<=10;j++)
+//     {
+//         console.log(i,j)
+//     }
+// }
+
+// ---------------------------------------------------------------------------------------------------
+//write a program to print 10 tables?
+
+// for (i=2;i<=5;i++)
+// {
+//     console.log(`------------${i} Table---------------`)
+//     for (j=0;j<=10;j++)
+//     {
+//         console.log(`${i} x ${j} = ${i*j}`)
+//     }
+// }
+
+// while loop-------------------------------------------------------------------------------------------
+// var start=10,end=20 //start the value from 10 and till 20 after each internal loop satisfies
+
+// while(start<=end) 
+// //check the condition, if satisfies then enter the inner loop.
+// {
+//     i=1 // initialise new var
+//     while (i<=10) // checks the cond
+//     {
+//         console.log(`${start} x ${i} = ${start*i}`)// prints the output with all calculations
+//         i++// increments for seconds inner iteration
+// //     }start++// increments for second outer iteration
+// // }
+
+
+// let a = 'code'
+// for (i=0;i<a.lenth;i++)
+// {
+//     console.log(a[i])
+// }
+
+// var start=2,end=5
+
+// s=''
+// for (i=start;i<=end;i++)
+// {
+//     s=s+i+' '
+// }
+// console.log(s)
+
+// var a = 'code'-------------------------Alternative----------------------
+// c=''
+// for (i=1;i<a.length;i++)
+// {
+//    c=c+'**'+a[i]
+// }
+// console.log(a[0]+c)
+
+// var a=''
+// for (i=1;i<=5;i++)
+// {
+//     a=a+'*'
+//     console.log(a)
+// }
+
+
+var a='1'
+for (i=1;i<=5;i++)
+{
+    a=a+i
+    console.log(a)
+}
