35b99ae93cc5058daad647169cd94575c44ca101
rafi0117 <mohdshabaaz953@gmail.com>
2022-07-29 10:24:18 +0530

    patterns

---
+++ Iteration/16.js
@@ -0,0 +1,171 @@
+// let n = 5;
+// let string = "";
+// // External loop
+// for (let i = 1; i <= n; i++) {
+//   // printing spaces
+//   for (let j = 1; j <= n - i; j++) {
+//     string += " ";
+//   }
+//   // printing star
+//   for (let k = 0; k < 2 * i - 1; k++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+// let n = 5;
+// let string = "";
+// for (let i = 0; i < n; i++) {
+//   // printing star
+//   for (let k = 0; k < n - i; k++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+// let n = 5;
+// let string = "";
+// // Upside pyramid
+// for (let i = 1; i <= n; i++) 
+// {
+//   // printing spaces
+//   for (let j = n; j > i; j--) 
+//   {
+//     string += " ";
+//   }
+//   // printing star
+//   for (let k = 0; k < i * 2 - 1; k++) 
+//   {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// // downside pyramid
+// for (let i = 1; i <= n - 1; i++) {
+//   // printing spaces
+//   for (let j = 0; j < i; j++) {
+//     string += " ";
+//   }
+//   // printing star
+//   for (let k = (n - i) * 2 - 1; k > 0; k--) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+// let n = 5;
+// let string = "";
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 0; j < i; j++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// for (let i = 1; i <= n - 1; i++) {
+//   for (let j = 0; j < n - i; j++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+// let n = 5;
+// let string = "";
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 0; j < n - i; j++) {
+//     string += " ";
+//   }
+//   for (let k = 0; k < i; k++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// for (let i = 1; i <= n - 1; i++) {
+//   for (let j = 0; j < i; j++) {
+//     string += " ";
+//   }
+//   for (let k = 0; k < n - i; k++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+// -------------------------------------------------------------------------------------------------------------------------
+
+// let a = 10
+// let b = 0
+// for (i=0;i<=a;i++)
+// {
+//     for (j=10;j<=b;j--)
+//     console.log(a,b)
+// }
+
+
+// for ( i=1;i<=10;i++)
+// {
+//     console.log(i,11-i)
+
+// }
+// // console.log(i)
+
+
+// for (;i<=50;i++)
+// {
+//     console.log(i,11-i)
+// }
+
+// for (i=1,j=10;i<=10,j>=1;i++,j--)
+// {
+//     console.log(i,j)
+// }
+
+
+// let a = ""
+// count = 0
+// for (let i=1;i<=10;i++)
+// {
+//     a = a + i
+//     console.log(a)
+//     count ++
+// }
+// console.log(count)
+
+// let n = 5
+// let a = ''
+// for (let i=5;i>=1;i--)
+// {
+//     a=""
+//     for (j=i;j>=1;j--)
+//        {
+//         a = a+'* '
+//        } 
+//        console.log(a)
+// }
+
+// let a = ""
+// for (let i=1;i<=5;i++)
+// {
+//     a = ""
+//     for (j=0;j<=5;j++)
+//     {
+//         a=a+"* "
+//     }console.log(a)
+// }
+
+
+let n = 5;
+let a = "";
+for (let i = 1; i < n; i++) {
+  for (let j = 0; j < n - i; j++) {
+    a = a  + " ";
+  }
+  for (let k = 0; k < i; k++) {
+    a = a +"*";
+  }
+  a += "\n";
+}
+console.log(a);
